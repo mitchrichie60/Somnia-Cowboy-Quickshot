e190818869605941044d7d2994c3bbde37aeacd2
mitchrichie60 <mitchrichie60@gmail.com>
2025-08-09 19:39:00 +0700

    Unity Game Upload New
    Unity Game Upload New

---
+++ Assets/BlockchainScripts/GameConfig.cs
@@ -0,0 +1,12 @@
+﻿public static class GameConfig
+{
+    // Địa chỉ contract ERC721
+    public static readonly string NFTTokenGateContractAddress = "0xDbbEE8807a6C60BfAA98828EA7E307B1D377d1C1";
+
+    // Địa chỉ contract ERC721 khác (nếu khác với TokenGate)
+    public static readonly string NFTVIPContractAddress = "0xDBa18C7e3EAA77666CAE549BE165b01f158c6A0A";
+
+    // Địa chỉ contract ERC20 - GEM Token
+    public static readonly string TokenGemContractAddress = "0x0e31639a5FeA0181255CA78a6205cd32AE862F5D";
+
+}
